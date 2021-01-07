5c6ef442f726250d0ffcaee4dc503e9cbbd3ca05
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2021-01-06 16:53:58 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ setInterval(gameUpdate,1000);
 function drawStuff() {
     ctx.fillStyle = "lime";
     ctx.fillRect(0,0,cvs.clientWidth,cvs.height);
+
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0,125,175,50);
 }
 
 function gameUpdate() {
