86f258125f957891d4b498593348e9237af08a3f
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2021-01-06 16:41:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ var ctx = cvs.getContext("2d");
 
 setInterval(gameUpdate,50);
 
-function gameUpdate() {
+function drawStuff() {
+    ctx.fillRect(0,0,cvs.clientWidth,cvs.height)
+}
 
+function gameUpdate() {
+    drawStuff();
 }
